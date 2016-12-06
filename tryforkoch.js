47613dd5511a7cda4f5c0335caaefc7366be95fa
Chris <shuvarek@gmail.com>
2016-12-06 03:37:49 +0000

    works better
    the size of the mountain in now proper (sqrt(3) )

--- tryforkoch.js
+++ tryforkoch.js
@@ -1,20 +1,21 @@
 // parameters
-let p1x = 100;
-let p1y = 200;
+let p1x = 200;
+let p1y = 200 * Math.sqrt(3);
 
 // from page
 var main_svg = document.getElementById("main_svg");
 
 
 // counted
+const wspolcz = Math.sqrt(3)/6;
 let p2x = p1x / 3;			// 1/3 the line
 let p2y = p1y / 3;
 let p3x = p1x * 2 / 3;		// 2/3 the line
 let p3y = p1y * 2 / 3;
 let p4x = p1x / 2;			// middle of the line
 let p4y = p1y / 2;
-let p5x = p4x + p2y;		// the interesting point
-let p5y = p4y - p2x;
+let p5x = p4x + (p1y * wspolcz);		// the interesting point
+let p5y = p4y - (p1x * wspolcz);
 
 
 let todraw = `
