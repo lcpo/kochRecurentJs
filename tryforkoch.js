ac22ab54c4d7fecfa50539c1a4bf563cfa823c8d
Chris <shuvarek@gmail.com>
2016-12-08 21:21:39 +0000

    demonstrate two wersions of drawing Koch Snowflake
    one is made from many (thousand and so, depends of deep) separate path,
    the other is one path, but little bit more sophisticated
    both demonstrate how to use the same library kochRecurent

--- tryforkoch.js
+++ tryforkoch.js
@@ -2,32 +2,67 @@
 
 
 // main program
-const main_svg = document.getElementById("main_svg");
+const svg1 = document.getElementById("svg1");
+const svg2 = document.getElementById("svg2");
 
-function main() {
-	let draw = '';
+let draw = '';
+let array_xy = [];
 
-	function drawit(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y) {
+function drawit1(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y) {
 //		console.log(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y);
-		let todraw = `
-			<path 
-				style="fill:none; stroke:#00f; stroke-width:1; stroke-opacity:1"
-				d="M ${k0x},${k0y}
-					L ${k1x},${k1y}
-					L ${k2x},${k2y}
-					L ${k3x},${k3y}
-					L ${k4x},${k4y}
-				"
-			/>
-		`;
-		draw += todraw;
-	}  // function drawit
+	let todraw = `
+		<path 
+			style="fill:none; stroke:#00f; stroke-width:1; stroke-opacity:1"
+			d="M ${k0x},${k0y}
+				L ${k1x},${k1y}
+				L ${k2x},${k2y}
+				L ${k3x},${k3y}
+				L ${k4x},${k4y}
+			"
+		/>
+`;
+	draw += todraw;
+}  // function drawit
 
-	let myheight = 300 * Math.sqrt(3) / 2;
-	kochRecurent(4, drawit, 100,100, 400,100);
-	kochRecurent(4, drawit, 400,100, 250,100+myheight);
-	kochRecurent(4, drawit, 250,100+myheight, 100,100);
-	main_svg.innerHTML = draw;
+
+function fordrawit2(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y) {
+	array_xy.push(k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y);
 }
 
-main();
+function drawit2(startx,starty,myarray_xy) {
+	todraw = `
+		<path 
+			style="fill:#ff0; stroke:#00f; stroke-width:0;"
+			d="M ${startx},${starty}
+	`;
+	let array_xy_length = myarray_xy.length;
+	for (let i = 0; i < array_xy_length ; i+=2 ) {
+		todraw += `L ${myarray_xy[i]},${myarray_xy[i+1]} `;
+	}
+	todraw += `
+			z"
+		/>
+	`;
+	return todraw;
+}
+
+let myheight = 300 * Math.sqrt(3) / 2;
+
+// first pic
+kochRecurent(4, drawit1, 100,100, 400,100);
+kochRecurent(4, drawit1, 400,100, 250,100+myheight);
+kochRecurent(4, drawit1, 250,100+myheight, 100,100);
+svg1.innerHTML = draw;
+
+// second pic
+kochRecurent(4, fordrawit2, 100,100, 400,100);
+kochRecurent(4, fordrawit2, 400,100, 250,100+myheight);
+kochRecurent(4, fordrawit2, 250,100+myheight, 100,100);
+svg2.innerHTML = drawit2(100,100, array_xy);
+
+
+
+
+
+
+
