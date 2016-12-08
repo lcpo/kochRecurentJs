cbacdaf339f85263364206cb4b2377d9f831e5d4
Chris <shuvarek@gmail.com>
2016-12-08 09:50:41 +0000

    extracted the kochRecurent function out of the main body

--- tryforkoch.js
+++ tryforkoch.js
@@ -1,10 +1,7 @@
 // tryforkoch.js
 
 
-// global declarations
-const main_svg = document.getElementById("main_svg");
-let draw = '';
-
+// to be extracted to outside library
 function kochRecurent(depth, funct, point0x, point0y, point1x, point1y) {
 	const wspolcz = Math.sqrt(3)/6;
 	
@@ -37,24 +34,34 @@ function kochRecurent(depth, funct, point0x, point0y, point1x, point1y) {
 }
 
 
-function drawit(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y) {
-	console.log(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y);
-	let todraw = `
-		<path 
-			style="fill:none; stroke:#00f; stroke-width:1; stroke-opacity:1"
-			d="M ${k0x},${k0y}
-				L ${k1x},${k1y}
-				L ${k2x},${k2y}
-				L ${k3x},${k3y}
-				L ${k4x},${k4y}
-			"
-		/>
-	`;
-	draw += todraw;
+
+// main program
+const main_svg = document.getElementById("main_svg");
+
+function main() {
+	let draw = '';
+
+	function drawit(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y) {
+//		console.log(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y);
+		let todraw = `
+			<path 
+				style="fill:none; stroke:#00f; stroke-width:1; stroke-opacity:1"
+				d="M ${k0x},${k0y}
+					L ${k1x},${k1y}
+					L ${k2x},${k2y}
+					L ${k3x},${k3y}
+					L ${k4x},${k4y}
+				"
+			/>
+		`;
+		draw += todraw;
+	}  // function drawit
+
+	let myheight = 300 * Math.sqrt(3) / 2;
+	kochRecurent(4, drawit, 100,100, 400,100);
+	kochRecurent(4, drawit, 400,100, 250,100+myheight);
+	kochRecurent(4, drawit, 250,100+myheight, 100,100);
+	main_svg.innerHTML = draw;
 }
 
-let myheight = 300 * Math.sqrt(3) / 2;
-kochRecurent(4, drawit, 100,100, 400,100);
-kochRecurent(4, drawit, 400,100, 250,100+myheight);
-kochRecurent(4, drawit, 250,100+myheight, 100,100);
-main_svg.innerHTML = draw;
+main();
