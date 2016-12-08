664f3138866878a1543302a159bb6fcb216dbfcd
Chris <shuvarek@gmail.com>
2016-12-08 00:26:37 +0000

    try with console.log
    ony put things into console.log to see if the idea is working at all

--- tryforkoch.js
+++ tryforkoch.js
@@ -5,8 +5,9 @@
 const main_svg = document.getElementById("main_svg");
 
 
-function makeKochLinePath(point0x, point0y, point1x, point1y) {
+function kochRecurent(depth, funct, point0x, point0y, point1x, point1y) {
 	const wspolcz = Math.sqrt(3)/6;
+	
 	let p1x = point1x - point0x;
 	let p1y = point1y - point0y;
 	let p2x = p1x / 3;			// 1/3 the line
@@ -23,6 +24,33 @@ function makeKochLinePath(point0x, point0y, point1x, point1y) {
 	let point3y = point0y + p3y;
 	let point5x = point0x + p5x;
 	let point5y = point0y + p5y;
+
+	if (depth <= 0) {
+		funct(point0x, point0y, point2x, point2y, point5x, point5y, point3x, point3y, point1x, point1y);
+	} else {
+		depth--;
+		kochRecurent(depth, funct, point0x, point0y, point2x, point2y);
+		kochRecurent(depth, funct, point2x, point2y, point5x, point5y);
+		kochRecurent(depth, funct, point5x, point5y, point3x, point3y);
+		kochRecurent(depth, funct, point3x, point3y, point1x, point1y);
+	}
+}
+
+
+function logpoints(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y) {
+	console.log(k0x,k0y,k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y);
+}
+
+
+kochRecurent(2, logpoints, 200,150, 300,500);
+
+
+
+
+
+
+
+/*
 	let todraw = `
 		<path 
 			id="path1"
@@ -38,4 +66,5 @@ function makeKochLinePath(point0x, point0y, point1x, point1y) {
 	return todraw;
 }
 
-main_svg.innerHTML = makeKochLinePath(200,150, 300,500);
+main_svg.innerHTML = kochRecurent(2, funct, 200,150, 300,500);
+*/
